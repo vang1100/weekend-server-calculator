ba4f505504b56c9eaa7c2ae8b74511ef61a2b67c
Padee Vang <vangpadee11@gmail.com>
2024-07-27 16:03:16 -0500

    calc history is on DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,10 +10,22 @@ function getCalculations(){
     axios.get('/calculations').then(response => {
         //console.log(response);
         console.log('grabbing the data', response.data);
+
+       console.log(renderHistory(response.data));
+
     }).catch(error => {
         console.log('Axios GET failed, unable to load calculations array', error);
     })
 
 }
 
+function renderHistory(calculations){
+    let contentDiv = document.querySelector('#history');
+  //  contentDiv.innerHTML = '';
+ 
+  for( let items of calculations) {
+    contentDiv.innerHTML += `<ul> <li>${items.numOne} ${items.operator} ${items.numTwo} = ${items.result} </li></ul> <br>`
+  }
+
+}
 getCalculations();
